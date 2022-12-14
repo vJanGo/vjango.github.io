1b877745ab9ebcebd85e2a69fa5721bac3de811b
vjango <3557458017@qq.com>
2022-12-15 00:07:45 +0800

    Site updated: 2022-12-15 00:07:44

--- sourceCode.js
+++
@@ -1,7 +0,0 @@
-import PoorbugCommenter from '../src/entry'
-
-const poorbugCommenter = new PoorbugCommenter({
-    id: 'yYInORSnBNBQQFefrizUTyT3-gzGzoHsz',
-    key: 'B8TjOpTwOGBgAItLVx0nBIs3',
-    item: 'test'
-})
